1999296b810ec84ca71a0a1aada01661e4b9a027
DreamEcho100 <maze6572198@gmail.com>
2021-11-27 13:15:44 +0200

    - In backend: simple get requests with express.

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,21 @@
 import express from 'express';
+import products from './data/products';
 
 const app = express();
 const PORT = 5000;
 
 app.get('/', (req, res) => {
 	res.send('API is running....');
+});
+
+app.get('/api/v1/products', (req, res) => {
+	res.json(products);
+});
+
+app.get('/api/v1/products/:id', (req, res) => {
+	const product = products.filter((item) => item._id === req.params.id);
+
+	res.json(product);
 });
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
