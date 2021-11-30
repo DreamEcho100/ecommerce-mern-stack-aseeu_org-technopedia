142185771d6991d276b621612aa3e8dfefa85367
DreamEcho100 <maze6572198@gmail.com>
2021-11-30 16:15:29 +0200

    - In frontend: - Enabling Redux and Creating Redux store.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from 'src/store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -8,7 +11,9 @@ import App from './App';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
