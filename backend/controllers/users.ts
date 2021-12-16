3a3de7adc07103fe8e6ffa5dde0aefc343f280a3
DreamEcho100 <maze6572198@gmail.com>
2021-12-17 00:32:39 +0200

    New user registration

--- backend/controllers/users.ts
+++ backend/controllers/users.ts
@@ -41,4 +41,34 @@ const getUserProfile = asyncHandler(
 	}
 );
 
-export { authUser, getUserProfile };
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
+	const { name, email, password } = req.body;
+
+	const userExist = await UserModel.findOne({ email });
+
+	if (userExist) {
+		res.status(400);
+		throw new Error('User already exist');
+	}
+
+	const user = await UserModel.create({
+		name,
+		email,
+		password,
+	});
+
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id),
+		});
+	} else {
+		res.status(400);
+		throw new Error('Invalid user data');
+	}
+});
+
+export { authUser, getUserProfile, registerUser };
--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { authUser, getUserProfile } from '../controllers/users';
+import { authUser, getUserProfile, registerUser } from '../controllers/users';
 import { protectMiddleware } from '../middleware/auth';
 
 const usersRoutes = express.Router();
 
+usersRoutes.route('/').post(registerUser);
 usersRoutes.post('/login', authUser);
 usersRoutes.route('/profile').get(protectMiddleware, getUserProfile);
 
