e5357bf8701ab6b62488c95ba8cbf33a094d5deb
DreamEcho100 <maze6572198@gmail.com>
2022-04-21 02:55:43 +0200

    Styles changes.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import './index.css';
 import './assists/BootstrapThemes/LUXBootstrap.min.css';
+import './index.css';
 
 import { Provider } from 'react-redux';
 import store from '@src/store';
